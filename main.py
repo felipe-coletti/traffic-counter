e0f909585d08fe4c91df8d5522e17ddf494afc33
felipe-coletti <felipecoletti@proton.me>
2026-08-03 16:01:30 -0300

    feat: add vehicle tracking logic to capture and display vehicle positions

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ lines = [
     ('Street 6', (275, 300), (100, 425)),
 ]
 
+vehicle_tracks = {}
+
 while cap.isOpened():
     ret, frame = cap.read()
 
@@ -47,6 +49,30 @@ while cap.isOpened():
             (0, 255, 255),
             2
         )
+
+    boxes = results[0].boxes
+
+    if boxes.id is not None:
+        for box in boxes:
+            if box.id is None:
+                continue
+
+            track_id = int(box.id.item())
+
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+            cx = (x1 + x2) // 2
+            cy = (y1 + y2) // 2
+
+            vehicle_tracks[track_id] = (cx, cy)
+
+            cv2.circle(
+                annotated_frame,
+                (cx, cy),
+                5,
+                (0, 0, 255),
+                -1
+            )
 
     cv2.imshow('Traffic Counter', annotated_frame)
 
